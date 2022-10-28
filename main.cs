b3b75c8f29351da59918f376fd350f9a7ff0d8e0
MikeyGloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-10-28 12:22:17 -0400

    code change

--- main.cs
+++ main.cs
@@ -1,16 +1,34 @@
 // Created by: Mikey Gloriani
 // Created on: Oct 2022
 //
-// This program displays, "Hello, World!"
+// This program displays the area of a trapezoid
 
 
 using System;
 
-class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+class Program {
+    public static void Main (string[] args) {
+        // This function accepts user input
+        int aBase;
+        int bBase;
+        int height;
+        int area;
+
+        Console.WriteLine("This program calculates the area of a trapezoid.");
+        Console.WriteLine("");
+
+        Console.Write("Enter the base for A (cm): ");
+        aBase = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the base for B (cm): ");
+        bBase = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the height (cm): ");
+        height = Convert.ToInt32(Console.ReadLine());
+
+        area = ((aBase+bBase)/2)*height;
+
+        Console.WriteLine("");
+        Console.WriteLine("The area is: " + area + " cm². ");
+
         Console.WriteLine("\nDone.");
     }
 }
